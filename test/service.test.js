118968e848ad58b2631358b766b2cf6055160304
mtso <mtso@tuta.io>
2017-03-20 23:40:36 -0700

    Complete natural format parsing

--- test/service.test.js
+++ test/service.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
+const service = require('../service');
 
 describe('GET /', function() {
   it('understands a unix timestamp', function(done) {
-    request
+    request(service)
       .get('/1450137600')
-      .expect()
       .expect('Content-Type', /json/)
       .expect(200, {
         unix: 1450137600,
@@ -13,10 +13,32 @@ describe('GET /', function() {
   });
 
   it('understands a natural language date', function(done) {
-    done(new Error('not implemented'));
+    request(service)
+      .get('/December%2015,%202015')
+      .expect('Content-Type', /json/)
+      .expect(200, {
+        unix: 1450137600,
+        natural: "December 15, 2015"
+      }, done);
   });
 
   it('does not understand other values', function(done) {
-    done(new Error('not implemented'));
+    request(service)
+      .get('/cryptocat')
+      .expect('Content-Type', /json/)
+      .expect(200, {
+        unix: null,
+        natural: null
+      }, done);
+  });
+
+  it('understands weird numbers', function(done) {
+    request(service)
+      .get('/123')
+      .expect('Content-Type', /json/)
+      .expect(200, {
+        unix: 123,
+        natural: 'January 1, 1970'
+      }, done);
   });
 });
